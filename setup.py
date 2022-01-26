454b53bbb5669cc4f26f39da433f766763ddd284
OrigamiDream <sdy36071@naver.com>
2022-01-26 23:31:12 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(name='trans-unet',
+      version='0.0.1',
+      description='An ML model with U-shaped architecture with ResNet50V2 and Vision Transformer based encoders',
+      url='https://github.com/Basars/trans-unet.git',
+      author='OrigamiDream',
+      packages=find_packages(),
+      zip_safe=False,
+      include_package_data=True,
+      install_requires=[
+          'tensorflow>=2.0',
+          'numpy',
+          'scipy'
+      ],
+      extra_require={
+          'tests': ['pytest']
+      })
