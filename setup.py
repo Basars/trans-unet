7c48f0abebc4b3898a545cb8e3d0ae86cb5cc820
OrigamiDream <sdy36071@naver.com>
2022-01-27 11:39:18 +0900

    Update to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(name='trans-unet',
-      version='0.0.1',
+      version='0.0.2',
       description='An ML model with U-shaped architecture with ResNet50V2 and Vision Transformer based encoders',
       url='https://github.com/Basars/trans-unet.git',
       author='OrigamiDream',
